ea15fb3eb053c890951d7782c954c81f8759c6ed
Mickael van Schie <mickael.v.s.19@hotmail.com>
2025-01-21 13:32:28 +0100

    api config

--- src/phpBunniClient.php
+++ src/phpBunniClient.php
@@ -4,5 +4,29 @@ namespace phpBunniApi\Api;
 
 class phpBunniApi
 {
+	public const $apiKey = null;
+	public const $businessId = null;
+	public const $apiUrl = null;
 
-}
+
+	public function setApiKey($apiKey)
+	{
+		$this->apiKey = $apiKey;
+
+		return $this;
+	};
+
+	public function setBusinessId($businessId)
+	{
+		$this->businessId = $businessId;
+
+		return $this;
+	};
+
+	public function buildApiUrl()
+	{
+		$this->apiUrl = "https://api.bunni.nl/" . $this->apiKey . '/' . $this->businessId;
+
+		return $this->apiUrl;
+	}
+};
