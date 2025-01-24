00efc66cff9f255f95473667829d7d95e2084366
unknown <mickael.v.s.19@hotmail.com>
2025-01-24 11:23:13 +0100

    httpRequests now work

--- src/phpBunniClient.php
+++ src/phpBunniClient.php
@@ -2,31 +2,56 @@
 
 namespace phpBunniApi\Api;
 
+
+use phpBunni\Api\Exception\ApiException;
+
 class phpBunniApi
 {
-	public const $apiKey = null;
-	public const $businessId = null;
-	public const $apiUrl = null;
+    public $apiKey = null;
+    public $businessId = null;
+    public $apiUrl = null;
+
+    public $apiVersion = "0.1";
 
 
-	public function setApiKey($apiKey)
-	{
-		$this->apiKey = $apiKey;
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
 
-		return $this;
-	};
+        return $this;
+    }
 
-	public function setBusinessId($businessId)
-	{
-		$this->businessId = $businessId;
+    public function setBusinessId($businessId)
+    {
+        $this->businessId = $businessId;
 
-		return $this;
-	};
+        return $this;
+    }
 
-	public function buildApiUrl()
-	{
-		$this->apiUrl = "https://api.bunni.nl/" . $this->apiKey . '/' . $this->businessId;
+    public function buildApiUrl()
+    {
+        $this->apiUrl = "https://api.bunni.nl/" . $this->apiVersion . "/" . $this->businessId . "/";
 
-		return $this->apiUrl;
-	}
-};
+        return $this->apiUrl;
+    }
+
+    protected function buildHeaders() {
+        return stream_context_create(['http' => ['header' => array(
+            "Accept: application/json", "Authorization: Bearer " . $this->apiKey, "Content-Type: application/json")]]);
+    }
+
+    public function createHttpRequest($endpoint, $data, $method = "GET")
+    {
+        $apiKey = $this->apiKey;
+        $businessId = $this->businessId;
+        if (!isset($apiKey)) {
+            // todo: throw exception.
+            echo("apkikey not set");
+        }
+        if (!$businessId) {
+            // todo: throw exception.
+            echo("businessId not set");
+        }
+        return file_get_contents($this->buildApiUrl() . $endpoint, false, $this->buildHeaders());
+    }
+}
